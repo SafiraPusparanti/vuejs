1c84ac8c4edc9a813656c97f7ae25d630dbef687
safira.pusparanti <safira.pusparanti@gmail.com>
2018-01-12 05:58:43 +0700

    first commit

---
+++ src/components/Link.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="links">
+    <router-link to="/">Home</router-link>
+    <router-link to="/customer-list">Customers</router-link>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'Link',
+    data () {
+      return {
+        msg: 'This is amazing'
+      }
+    }
+  }
+</script>
---
+++ src/pages/CustomerList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div>
+      Name: <input type="text" v-model="nameCustomer" placeholder="Type">
+
+    </div>
+    <table align="center">
+      <tr>
+        <th>Name</th>
+      </tr>
+      <tr v-for="d in data" :key="d.id">
+        {{d.name}}
+      </tr>
+    </table>
+  </div>
+</template>
+<script>
+  import axios from 'axios'
+  export default {
+    name: 'customer-list',
+    data () {
+      return {
+        nameCustomer: '',
+        addressCust: '',
+        data: null
+      }
+    },
+    methods: {
+      getUsers () {
+        const URL = '/api/customers'
+        axios.get(URL)
+          .then(response => {
+            this.data = response.data
+            alert(JSON.stringify(this.data))
+          })
+          .catch(error => {
+            alert(error)
+          })
+      }
+    },
+    mounted () {
+      this.getUsers()
+    }
+  }
+</script>
+<style>
+</style>
